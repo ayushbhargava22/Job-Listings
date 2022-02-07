f7ca5ced9e267ab6803f077f5b033490f3f14cee
ayush bhargava <57057106+ayushbhargava22@users.noreply.github.com>
2022-02-07 16:58:31 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 //selecting dom elements
 const jobContainer = document.querySelector(".job-listing");
+const searchContainer = document.querySelector(".search-bar");
+const searchContent = document.querySelector(".search-container");
+
 // Function for fetching data
 const fetchData = async () => {
   const res = await fetch("./data.json");
@@ -11,7 +14,7 @@ const fetchData = async () => {
 //Make cards/list
 const makeCards = (item) => {
   return `
-   <div class = "job featured-job">
+   <div class = "${item.featured === true ? "job featured" : "job"}">
    <div class="job-description">
    <img
      alt="Job logo"
@@ -19,7 +22,11 @@ const makeCards = (item) => {
      src=${item.logo}
    />
    <div class="job-name">
-     <p class="company-name">${item.company}</p>
+     <div class="job-tags-name">
+      <p class="company-name">${item.company}</p>
+      ${item.new === true ? '<p class="new-tag">New</p>' : ""}
+      ${item.featured === true ? '<p class="featured-tag">Featured</p>' : ""}
+     </div>
      <p class="job-position">${item.position}</p>
      <div class="deadline">
        <ul class="job-duration">
@@ -32,8 +39,8 @@ const makeCards = (item) => {
  </div>
  <div class="job-tags">
    <ul class="tag-container">
-     <li class="tag-list">${item.role}</li>
-     <li class="tag-list">${item.level}</li>
+     <li class="tag-list filter">${item.role}</li>
+     <li class="tag-list filter">${item.level}</li>
      ${createLang(item.languages)}
      ${createTools(item.tools)}
    </ul>
@@ -58,7 +65,7 @@ showCards();
 const createLang = (data) => {
   let langsCard = "";
   data.forEach((item) => {
-    langsCard += `<li class="tag-list">${item}</li>`;
+    langsCard += `<li class="tag-list filter">${item}</li>`;
   });
   return langsCard;
 };
@@ -66,7 +73,67 @@ const createLang = (data) => {
 const createTools = (data) => {
   let toolsCard = "";
   data.forEach((item) => {
-    toolsCard += `<li class="tag-list">${item}</li>`;
+    toolsCard += `<li class="tag-list filter">${item}</li>`;
   });
   return toolsCard;
 };
+
+//function to display search bar
+const showSearch = (e) => {
+  if (e.target.classList.contains("filter")) {
+    searchContainer.classList.remove("hidden");
+    displayFilter(e.target);
+  }
+};
+
+let filterArray = [];
+const displayFilter = (ele) => {
+  let filter = "";
+  if (!filterArray.includes(ele.textContent)) {
+    filterArray.push(ele.textContent);
+  }
+
+  filterArray.forEach((element) => {
+    filter += `
+      <div class="search-tag">
+         <p class="tag-names">${element}</p>
+         <span class="delete-tag">╳</span>
+      </div>
+     `;
+    searchContent.innerHTML = filter;
+    filterJob();
+  });
+};
+
+const filterJob = (data) => {
+  if (filterArray.length !== 0) {
+    fetchData().then((data) => {
+      let cards = "";
+      data.forEach((text) => {
+        if (validJobs(text)) {
+          cards += makeCards(text);
+          jobContainer.innerHTML = cards;
+        }
+      });
+    });
+  }
+};
+
+// Jobs are valid or not
+const validJobs = (item) => {
+  let isValid = true;
+  filterArray.forEach((elem) => {
+    if (
+      item.role !== elem &&
+      item.level !== elem &&
+      !item.languages.includes(elem) &&
+      !item.tools.includes(elem)
+    ) {
+      isValid = false;
+    }
+  });
+  return isValid;
+};
+
+// event to dislay search bar
+jobContainer.addEventListener("click", showSearch);
